c2cc3e6d08c31e67fad1a7d700908f5a1a0d3db0
Mirza <mirza_shamsudin@hotmail.com>
2023-01-15 05:30:58 +0800

    Update basta.py

--- basta.py
+++ basta.py
@@ -33,3 +33,30 @@
 # 16.) Create a new franchise with an address of "189 Fitzgerald Avenue". Save the franchise to a variable called arepas_place.
 
 # 17.) Create a new business called "Take a' Arepa". Repeat the process as required.
+
+brunch_items = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 
+  'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 
+  'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+  }
+
+early_bird_items = {
+  'salumeria plate': 8.00, 
+  'salad and breadsticks (serves 2, no refills)': 14.00, 
+  'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 
+  'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 
+  'espresso': 3.00
+  }
+
+dinner_items = {
+  'crostini with eggplant caponata': 13.00, 'caesar salad': 16.0,
+  'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 
+  'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00
+  }
+
+kids_items = {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 
+  'apple juice': 3.00
+  }
+
+# Start writing your code below -------------------------------------------------------
